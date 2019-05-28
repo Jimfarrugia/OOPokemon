155a362f602ece677d6c9b2525704319295b76b7
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-05-28 20:39:25 +1000

    some console output and comments

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+/* 
+####  To Do  ####
+- [ ] Team class.  Group of pokemon(members).
+    - [ ] method to check hp of members (readyToFight=>array)
+- [ ] 
+- [ ] give pokemon xp bars and a method to gain xp
+- [ ] define max-xp, level-up a pokemon at max-xp
+*/
+
 import { types } from './_types';
 import { abilities } from './_abilities';
 import { Pokemon } from './_pokemon';
-
 
 class Bulbasaur extends Pokemon {
   constructor() {
@@ -17,13 +25,24 @@ class Charmander extends Pokemon {
   }
 }
 
-
 let charmander = new Charmander();
 console.log(charmander);
 
+let bulbasaur = new Bulbasaur();
+console.log(bulbasaur);
 
-// console.log(types);
-// console.log(abilities);
+console.log("*******************")
 
+bulbasaur.attack(charmander, 1);
+console.log(charmander.hp);
 
-//console.log(abilities);
+charmander.attack(bulbasaur, 0);
+console.log(bulbasaur.hp);
+
+bulbasaur.attack(charmander, 1);
+console.log(charmander.hp);
+
+charmander.attack(bulbasaur, 1);
+console.log(bulbasaur.hp);
+
+console.log("*******************")
