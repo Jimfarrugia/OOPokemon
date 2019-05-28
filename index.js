970908ed1f870359a41fecd49c2384363f904fd2
Jim Farrugia <the.jim.farrugia@gmail.com>
2019-05-28 20:56:25 +1000

    (WIP)created trainer class

--- index.js
+++ index.js
@@ -2,14 +2,22 @@
 ####  To Do  ####
 - [ ] Team class.  Group of pokemon(members).
     - [ ] method to check hp of members (readyToFight=>array)
-- [ ] 
+- [ ] add more pokemon instances (pidgey, weedle, rattata, psyduck, diglet)
+- [ ] add some trainers (team instances)
 - [ ] give pokemon xp bars and a method to gain xp
 - [ ] define max-xp, level-up a pokemon at max-xp
 */
-
 import { types } from './_types';
 import { abilities } from './_abilities';
 import { Pokemon } from './_pokemon';
+
+class Trainer {
+  constructor(name, team) {
+    this.name = name;
+    this.team = team;
+  }
+  
+}
 
 class Bulbasaur extends Pokemon {
   constructor() {
@@ -25,24 +33,52 @@ class Charmander extends Pokemon {
   }
 }
 
-let charmander = new Charmander();
-console.log(charmander);
+class Squirtle extends Pokemon {
+  constructor() {
+    super("Squirtle", "squii..!", types["water"]);
+    this.moveset.push(abilities["bubble"]);
+  }
+}
 
+console.log("*******************")
+
+let charmander = new Charmander();
+let squirtle = new Squirtle();
 let bulbasaur = new Bulbasaur();
+
+console.log("*******************")
+
+bulbasaur.attack(charmander, 1);
+
+console.log("*******************")
+
+console.log(charmander);
+console.log(squirtle);
 console.log(bulbasaur);
 
 console.log("*******************")
 
-bulbasaur.attack(charmander, 1);
 console.log(charmander.hp);
-
-charmander.attack(bulbasaur, 0);
-console.log(bulbasaur.hp);
-
-bulbasaur.attack(charmander, 1);
-console.log(charmander.hp);
-
-charmander.attack(bulbasaur, 1);
-console.log(bulbasaur.hp);
 
 console.log("*******************")
+
+let playerRed = new Trainer("Red", [new Squirtle(), new Charmander()]);
+console.log(playerRed);
+
+console.log("*******************")
+
+// Battle
+
+// console.log("*******************")
+
+
+// charmander.attack(bulbasaur, 0);
+// console.log(bulbasaur.hp);
+
+// bulbasaur.attack(charmander, 1);
+// console.log(charmander.hp);
+
+// charmander.attack(bulbasaur, 1);
+// console.log(bulbasaur.hp);
+
+// console.log("*******************")
